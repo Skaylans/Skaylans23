310f9028d7b32f8f6ba2807de82b252d3263822d
Skaylans <32162864+Skaylans@users.noreply.github.com>
2018-02-28 13:56:11 +0300

    Update exit.php

--- exit.php
+++ exit.php
@@ -1,7 +1,7 @@
 <?php
-unset($_COOKIE['user_id']);
+unset($_COOKIE['id']);
 unset($_COOKIE['username']);
-setcookie('user_id', '', -1, '/');
+setcookie('id', '', -1, '/');
 setcookie('username', '', -1, '/');
 $home_url = 'http://' . $_SERVER['HTTP_HOST'];
 header('Location: '. $home_url);
