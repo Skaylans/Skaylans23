ea287195d33a51dc4231ead8852ac493651284c6
Skaylans <32162864+Skaylans@users.noreply.github.com>
2018-02-28 13:17:52 +0300

    Create createtable.php

---
+++ createtable.php
@@ -0,0 +1,26 @@
+<?
+
+$dsn = "sqlsrv:server = tcp:safelife.database.windows.net,1433; Database = insurance";
+$login = "Romanow";
+$pass = "Qwerty123456";
+
+
+try {
+    $conn = new PDO($dsn, $login, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "CREATE TABLE registration_tab(
+        id INT NOT NULL IDENTITY(1,1), 
+        PRIMARY KEY(id),
+        name VARCHAR(30),
+        email VARCHAR(30),
+        gender VARCHAR(10),
+        reg_date DATE)";
+        $conn->query($sql);
+        
+        echo "<h3>Таблица создана.</h3>";
+    }
+catch (PDOException $e) {
+    print("Ошибка подключения к SQL Server.");
+    die(print_r($e));
+}
+?>
