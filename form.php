56f7dd0b982a7598853bce687b05a959be2cc01d
Skaylans <32162864+Skaylans@users.noreply.github.com>
2018-03-01 12:12:59 +0300

    Update form.php

--- form.php
+++ form.php
@@ -1,1 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/font-awesome.css">
+    <title>Страхование жизни</title>
+  </head>
+  <body>
+    <div class="container">
+      <img src="img/lock.png">
+      <form class="" action="index.php" method="post">
+        <div class="dws-input">
+          <input type="text" name="username" placeholder="Введите логин">
+        </div>
+        <div class="dws-input">
+          <input type="password" name="password" placeholder="Введите пароль">
+        </div>
+        <input class="dws-submit" type="submit" name="submit" value="Войти">
+      </form>
 
+    </div>
+  </body>
+</html>
