34fed0e2d58d46ff96a48b863bc5f09820743ad6
Skaylans <32162864+Skaylans@users.noreply.github.com>
2018-03-12 01:10:28 +0400

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 
-$dsn = "sqlsrv:server = tcp:safelife.database.windows.net,1433; Database = insurance";
+$dsn = "sqlsrv:server = tcp:safelife.database.windows.net,1433; Database = Insurance";
 $login = "Romanow";
 $pass = "Qwerty123456";
 
